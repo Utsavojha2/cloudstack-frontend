90998a026e59412f5d3e76a6955f3ea488e27aa1
Utsavojha2 <utsavojha2@gmail.com>
2022-03-01 13:08:06 +0545

    Creating reusable design components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient()
+
+const TopProgressBar = dynamic(() => import('containers/Loading/ProgressBar'), {
+  ssr: false,
+})
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TopProgressBar />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
-
-export default MyApp
