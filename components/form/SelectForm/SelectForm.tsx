f62686a1893677c94d5506c76082016441dbb9de
Utsavojha2 <utsavojha2@gmail.com>
2022-03-02 16:02:39 +0545

    Setting up react hook form for handling all app forms

---
+++ components/form/SelectForm/SelectForm.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import { Controller, useFormContext } from 'react-hook-form'
+
+interface SelectFormProps<T> {
+  label: string
+  name: string
+  selectOptions: Readonly<Array<T>>
+  renderOption?: (
+    props: React.HTMLAttributes<HTMLLIElement>,
+    option: T
+  ) => React.ReactNode
+}
+
+export default function SelectForm<T extends { label: string }>({
+  label,
+  name,
+  selectOptions,
+  renderOption = (_props, option) => option.label,
+}: SelectFormProps<T>) {
+  const { control } = useFormContext()
+  return (
+    <Controller
+      render={(props) => (
+        <Autocomplete
+          {...props}
+          options={selectOptions}
+          getOptionLabel={(option) => option.label}
+          renderOption={renderOption}
+          renderInput={(params) => (
+            <TextField {...params} label={label} variant="outlined" />
+          )}
+          onChange={(_, data) => props.field.onChange(data)}
+        />
+      )}
+      name={name}
+      control={control}
+    />
+  )
+}
